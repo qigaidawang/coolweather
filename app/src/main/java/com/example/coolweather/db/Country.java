55f9127ee549aab15738f09b99c63a8eb7dde522
wenwen <1027060543@qq.com>
2019-03-28 19:03:24 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/coolweather/db/Country.java
@@ -0,0 +1,44 @@
+package com.example.coolweather.db;
+
+/**
+ * Created by wenwen on 2019/3/28.
+ */
+
+public class Country {
+    private int id;
+    private String countryName;
+    private  String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
